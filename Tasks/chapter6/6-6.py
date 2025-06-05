19f804dcfee2f774e04adeedc0212de84b5531f4
JosephGleason <9719@holbertonstudents.com>
2025-06-04 20:18:05 -0400

    addded chap 6 tasks

--- Tasks/chapter6/6-6.py
+++ Tasks/chapter6/6-6.py
@@ -8,3 +8,10 @@ favorite_languages = {
 
 for name, language in favorite_languages.items():
     print(f"{name.title()}'s favorite language is {language.title()}")
+
+coders = ['joe', 'heather', 'jen', 'phil']
+for coder in coders:
+    if coder in favorite_languages.keys():
+        print(f"Thank you for taking the poll, {coder.title()}!")
+    else:
+        print(f"{coder.title()}, what's your favorite programming language?")
