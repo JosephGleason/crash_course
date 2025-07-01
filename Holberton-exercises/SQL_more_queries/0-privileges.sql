d6dd1dbde4beea4225e5a34984dd31c838969e62
JosephGleason <9719@holbertonstudents.com>
2025-07-01 12:57:06 -0400

    added excersises

---
+++ Holberton-exercises/SQL_more_queries/0-privileges.sql
@@ -0,0 +1,7 @@
+-- 0. My privileges!
+
+-- Show grants for user_0d_1@localhost
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+
+-- Show grants for user_0d_2@localhost
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
